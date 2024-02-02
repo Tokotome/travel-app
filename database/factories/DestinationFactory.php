490c691a73fa5e61d3528598b84a3c57f2e93297
Tokotome <mandzhukov.bg@gmail.com>
2024-02-02 16:49:35 +0200

    create model, factory and seed data

---
+++ database/factories/DestinationFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\destination>
+ */
+class DestinationFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'country' => $this->faker->country,
+            'city' => $this->faker->city
+        ];
+    }
+}
---
+++ database/migrations/2024_02_02_143246_change_hotels.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('hotels', function(Blueprint $table) {
+            $table->integer('city')->references('city')->on('destinations')->restrictOnDelete()->change();
+            $table->integer('country')->references('country')->on('destinations')->restrictOnDelete()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
